334eaab7ea0a74abbd54b152d025732f86712dcc
spencerk226 <35508673+spencerk226@users.noreply.github.com>
2020-12-08 09:44:11 -0600

    Add day 8, will refactor

--- AdventOfCode/Runner.cpp
+++ AdventOfCode/Runner.cpp
@@ -5,10 +5,10 @@
 #include "DayFive.h"
 #include "DaySix.h"
 #include "DaySeven.h"
-
+#include "DayEight.h"
 #include <iostream>
 
 int main() {
 
-	printf("\n\n\n###############\nOutput: %d\n###############\n", daySeven2());
+	printf("\n\n\n###############\nOutput: %d\n###############\n", dayEight());
 }
