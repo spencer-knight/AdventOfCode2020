6b2bdf3ecaad3814c873fb3c592f6990d0e9bbab
spencerk226 <spencerk226@gmail.com>
2020-12-09 11:00:44 -0600

    Refactor Day 9 code

--- AdventOfCode/DayNine.cpp
+++ AdventOfCode/DayNine.cpp
@@ -5,36 +5,94 @@
 #include <sstream>
 #include <iostream>
 #include "DayNine.h"
+
+//Pass in cipher and position, and optionally preamble length. It will tell you if the number at that position is invalid.
+bool isInvalid(std::vector<int> &input, int pos, int preamble = 25) {
+
+	for (int i = std::max(0, pos - preamble); i < pos; i++) {
+
+		for (int j = std::max(0, pos - preamble); j < pos; j++) {
+
+			if (j != i && input[i] + input[j] == input[pos]) {
+
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+//given an invalid position, this will return the "weakness" of the cipher, which is just an int.
+int findWeakness(std::vector<int>& cipher, int invalid, int preamble = 25) {
+
+	for (int i = preamble; i < cipher.size(); i++) {
+
+		std::vector<int> nums;
+		int sum = 0;
+		for (int j = i; j < cipher.size() && sum < invalid; j++) {
+
+			sum += cipher[j];
+			nums.push_back(cipher[j]);
+			if (sum == invalid) {
+
+				return *std::min_element(nums.begin(), nums.end()) + *std::max_element(nums.begin(), nums.end());
+			}
+		}
+	}
+}
+
+//takes input and optionally preamble length and return the first invalid number position in cipher, doesn't check for any further ones. if none are found it returns -1;
+//returns the #### POSITION ####
+int findInvalid(std::vector<int> input, int preamble = 25) {
+
+	int invalid = -1;
+	for (int i = preamble; i < input.size() && invalid == -1; i++) {
+	
+		invalid = isInvalid(input, i, preamble) ? i : invalid;
+	}
+
+	return invalid;
+}
+
+//takes cipher input and optionally preamble length to return a list of invalid numbers, index 0 will be -1 if none are found
+//returns the #### POSITIONS ####
+//Seems to break on first day list because of integer overflow, hopefully this doesn't need to work on things that big
+std::vector<int> findInvalids(std::vector<int> input, int preamble = 25) {
+
+	bool noneFound = true;
+	std::vector<int> invalids;
+	for (int i = preamble; i < input.size(); i++) {
+
+		if (isInvalid(input, i, preamble)) {
+
+			invalids.push_back(i);
+		}
+	}
+	if (invalids.size() == 0) {
+
+		invalids.push_back(-1);
+	}
+
+	return invalids;
+}
+
+
 int part1(std::vector<int> input) {
 
 	int invalid = 0;
 	int preamble = 25;
 	int search = 25;
-	for (int i = preamble; i < input.size(); i++) {
+	for (int i = preamble; i < input.size() && invalid == 0; i++) {
 
-		bool found = false;
-		for (int j = std::max(0, i - search); j < input.size(); j++) {
+		printf("Checking %d\n", input[i]);
+		if (isInvalid(input, i)) {
 
-			for (int z = std::max(0, i - search); z < input.size(); z++) {
-
-				if (input[i] == input[j] + input[z] && i != j) {
-
-
-					found = true;
-					if (input[i] == 15690279) {
-
-						//printf("%d + %d = 15690279", input[j], input[z]);
-					}
-				}
-			}
-		}
-
-		if (!found) {
-
-			return input[i];
+			invalid = input[i];
+			break;
 		}
 	}
-	printf("part 1: %d\n", invalid);
+	//printf("part 1: %d\n", invalid);
+	return invalid;
 }
 int dayNine() {
 
@@ -49,32 +107,11 @@ int dayNine() {
 		input.push_back(push);
 		//printf("%s\n", line.c_str());
 	}
-	int invalid = part1(input);
-	printf("part1: %d\n", invalid);
-	for (int i = 0; i < input.size(); i++) {
-	
-		int sum = 0;
-		int j = i;
-		while (sum < invalid && j < input.size() && input[j] != invalid) {
 
-			sum += input[j];
-			//printf("%d\n", sum);
-			if (sum == invalid) {
-				
-				int min = 100000000;
-				int max = 0;
-				for (int z = i; z < j; z++) {
-					//printf("%d + ", input[z]);
-					min = std::min(min, input[z]);
-					max = std::max(max, input[z]);
-				}
-				//printf("= %d\n", invalid);
-				std::printf("part2: %d", min + max);
-				return 0;
-			}
 
-			j++;
-		}
-	}
+	int invalid = input[findInvalid(input)];
+	int weakness = findWeakness(input, invalid);
+	printf("Invalid: %d\nWeakness: %d\n", invalid, weakness);
+
 	return 0;
 }
