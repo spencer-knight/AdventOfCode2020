39cdb7fd79711fd5521940c66d987a3171678714
spencerk226 <spencerk226@gmail.com>
2020-12-20 16:56:57 -0600

    Day 19 part 2 finished by luck

--- AdventOfCode/DayNineteen.cpp
+++ AdventOfCode/DayNineteen.cpp
@@ -27,15 +27,26 @@ void makeRule(std::vector<std::string>& rules, std::vector<std::string>& sRules,
 
 	std::string buff;
 	std::vector<std::string> items = util::split(sRules[pos]);
+	std::string half;
+	bool split = false;
+	bool loop = false;
 	for (int i = 0; i < items.size(); i++) {
 
 		if (items[i] != "|") {
 
 			//std::cout << items[i] << std::endl;
 			int rule = stoi(items[i]);
+			//std::string half;
 			if (rules[rule] != "") {
 
 				buff += rules[rule];
+			}
+			else if (rule == pos) {
+
+				loop = true;
+				//half += "+";
+				//buff += half;
+				buff += ".";
 			}
 			else {
 
@@ -45,13 +56,42 @@ void makeRule(std::vector<std::string>& rules, std::vector<std::string>& sRules,
 		}
 		else {
 
-			buff += "|";
+		//	std::cout << buff << std::endl << std::endl;
+			half = "(" + buff + ")";
+			buff = "";
+			split = true;
+			//buff += "|";
 		}
 
-		//std::cout << buff;
+	//	std::cout << buff << std::endl;
 	}
 
-	rules[pos] = "(" + buff + ")";
+//	std::cout << buff << std::endl;
+	buff = "(" + buff + ")";
+	if (loop) {
+
+		int dot = util::indexOf('.', buff);
+		//std::cout << dot << std::endl;
+		std::string buffNoDot = buff.substr(0, dot) + half + "+|" + buff.substr(dot + 1) + "+";
+		std::string ruleNoDot = "(" + half + "|" + buffNoDot + ")";
+		std::string rec = ruleNoDot;
+		buff = buff.substr(0, dot) + rec + buff.substr(dot + 1);
+		std::string rule = "(" + half + "|" + buff + ")";
+		//std::string ruleNoDot = rule.substr(0, dot) + rule.substr(dot + 1);
+		//rule = rule.substr(0, dot) + ruleNoDot + rule.substr(dot + 1);
+		rules[pos] = rule;
+	}
+	//std::cout << buff << std::endl << std::endl;
+	else {
+
+		if (split) {
+			rules[pos] = "(" + half + "|" + buff + ")";
+		}
+		else {
+
+			rules[pos] = "(" + buff + ")";
+		}
+	}
 }
 int dayNineteen() {
 
@@ -61,37 +101,19 @@ int dayNineteen() {
 	std::vector <std::string> rules(138, "");
 	while (std::getline(file, line)) {
 
-	//	printf("\"%s\"\n", line.c_str());
-		// Put parsing shit here
 		if (line == "") {
 
 			break;
 		}
 		int colon = util::indexOf(':', line) + 1;
 		sRules[stoi(line.substr(0, colon - 1))] = (line.substr( colon + 1));
-		rules.push_back("");
 	}
 
 	for (int i = 0; i < sRules.size(); i++) {
 
-		//printf("%s\n", sRules[i].c_str());
-		makeRule(rules, sRules, i);
+		std::cout << i << ": " << sRules[i] << ";" << std::endl;
 	}
-
-	int sum = 0;
-	//this will get the strings to match
-	while (std::getline(file, line)) {
-
-		std::regex reg("^" + rules[0] + "$");
-		if (std::regex_match(line, reg)) {
-
-			//std::cout << line << std::endl;
-			sum++;
-		}
-	}
-
-	std::cout << "Part1: " << sum << std::endl;
-	return sum;
+	return 0;
 }
 int main() {
 
